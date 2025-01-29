fb0035911af8cec585baa6ccbd6c9f60f50d68a2
Basil Vazhathottathil <basilveldose@gmail.com>
2025-01-30 03:09:46 +0530

    built nav bar

--- my-app/app/components/ProductCard.tsx
+++ my-app/app/components/ProductCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Add from './Add';
-
+import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
       <Add/> 
     </div>
   )
